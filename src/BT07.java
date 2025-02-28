d8f263a5c08d1454b3511ca0d2010dac877f59db
nguyen xuan nhat <xuannhatvn211@gmail.com>
2025-02-28 17:03:01 +0700

    update

--- src/BT07.java
+++ src/BT07.java
@@ -1,24 +1,35 @@
 import java.util.Scanner;
 
-public class BT07 {
+public class BT07{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int gio = sc.nextInt();
-        int phut = sc.nextInt();
-        int giay = sc.nextInt();
-        if (gio < 0 || phut < 0 || phut >= 60 || giay < 0 || giay >= 60) {
+
+        System.out.print("Nhập giờ, phút, giây ban đầu: ");
+        int hour = sc.nextInt();
+        int minute = sc.nextInt();
+        int second = sc.nextInt();
+
+        if (hour < 0 || minute < 0 || minute >= 60 || second < 0 || second >= 60) {
             System.out.println("Lỗi: Thời gian nhập vào không hợp lệ!");
             return;
         }
-        int themgio = sc.nextInt();
-        int themphut = sc.nextInt();
-        int themgiay = sc.nextInt();
-        int tongsogiay = gio*3600 + phut*60 + giay;
-        int tongsogiaythem = themgio*3600 + themphut*60 + themgiay;
-        int tongthoigian = tongsogiay + tongsogiaythem;
-        int gio2 = tongthoigian / 3600;
-        int phut2 = (tongthoigian % 3600) / 60;
-        int giay2 = tongthoigian % 60;
-        System.out.println("thoi gian sau khi cong them la: "+gio2+" : "+phut2+" : "+giay2);
+
+        System.out.print("Nhập số giờ, phút, giây cần cộng thêm: ");
+        int addHour = sc.nextInt();
+        int addMinute = sc.nextInt();
+        int addSecond = sc.nextInt();
+
+        int newSecond = second + addSecond;
+        int newMinute = minute + addMinute;
+        int newHour = hour + addHour;
+        if (newSecond >= 60) {
+            newMinute += newSecond / 60;
+            newSecond %= 60;
+        }
+        if (newMinute >= 60) {
+            newHour += newMinute / 60;
+            newMinute %= 60;
+        }
+        System.out.printf("Thời gian sau khi cộng: %02d:%02d:%02d\n", newHour, newMinute, newSecond);
     }
 }
